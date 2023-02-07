51f8b137d7ba264c21ee7b6cb5daa1199ff48e1a
Karina Tavares <102122525+Karinatavaress@users.noreply.github.com>
2023-02-07 13:20:39 -0300

    commit init

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const menuLinks = document.querySelectorAll(".background a");
+function abrirmenu(){
+    let BarraMenuAberto = document.getElementById ('barra-menu')
+    if (BarraMenuAberto.style.width == "0px")
+    { BarraMenuAberto.style.width = "180px";
+    }else {
+        BarraMenuAberto.style.width = "0px";
+    }
 
-console.log(menuLinks);
+
+}
